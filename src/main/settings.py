88136ef8bd46d9dcc5d613047254e42041a93f64
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-02-19 02:56:30 +0800

    fix hosts

--- src/main/settings.py
+++ src/main/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-1k2eh-^_y5x4zjx1b!w)v^8(!&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['9mvjpd94ym.ap-southeast-1.awsapprunner.com', 'localhost','127.0.0.1']
 
 
 # Application definition
