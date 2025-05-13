1f0f86410a343e9e395cd36bc17920bfe06601bf
Maxell Milay <milaymaxell@gmail.com>
2025-05-14 01:21:03 +0800

    rearrange main urls

--- src/main/urls.py
+++ src/main/urls.py
@@ -24,7 +24,7 @@ def health_check(request):
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/ai/", include("ai.urls")),
     path("health/", health_check, name="health_check"),
+    path("api/ai/", include("ai.urls")),
     path("api/schedules/", include("schedules.urls")),
 ]
