3cd59dfec95b4af00a82fdf89b0e909d302e4133
cup-noodlehS <sheldonarthursagrado@gmail.com>
2025-05-28 18:01:31 +0800

    change throttle class

--- src/main/settings.py
+++ src/main/settings.py
@@ -75,8 +75,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day',
+        'anon': '60/minute',
+        'user': '120/minute',
     },
 }
 
